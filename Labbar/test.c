2b51476bdc5714d60295cd50098be1eaa27c7d4d
Pontuswis <42838246+Pontuswis@users.noreply.github.com>
2018-10-19 11:58:33 +0200

    Backup

--- Labbar/test.c
+++ Labbar/test.c
@@ -1,149 +1,340 @@
 #include <stdio.h>
-#include <math.h>
+#include <string.h>
+#include <stdlib.h>
+#define WORDLENGTH 30
+#define MAXPICTURES 10
+#define MAX 10000
 
-#define LENGTH 10
+struct patient{
+	char name[WORDLENGTH];
+	char idNum[WORDLENGTH];
+	int picRef[MAXPICTURES];
+	int nrOfPics;
+};
+typedef struct patient Patient;
 
-int enterData(int measurements[], int nrOfMeasurements);
-void viewData(int measurements[], int nrOfMeasurements);
-int reset(int measurements[], int nrOfMeasurements);
-void compute(int measurements[], int nrOfMeasurements);
-int calcMax(int measurements[], int nrOfMeasurements);
-int calcMin(int measurements[], int nrOfMeasurements);
-float calcAvr(int measurements[], int nrOfMeasurements);
-void copyArr(int measurements[], int tempArr[], int nrOfMeasurements);
-void calcNorm(int tempArr[], int nrOfMeasurements);
+void databaseManagement(Patient patientDatabase[], int *pNrOfPatients,char fileName[]);
+void regPatients(Patient patientDatabase[] , int *pNrOfPatients);
+Patient createPatient(char name[], char idNum[], int picRef[], int nrOfPics);
+void printDatabase(Patient patientDatabase[], int *pNrOfPatients);
+void printPatient(Patient p);
+void printMenu();
+int searchSelect(Patient patientDatabase[], int *pNrOfPatients);
+void searchName(Patient patientDatabase[], int *pNrOfPatients, int *pNrOfPatientsFound, int *pPatientFound);
+void searchId(Patient patientDatabase[], int *pNrOfPatients, int *pNrOfPatientsFound, int *pPatientFound);
+void searchPicRef(Patient patientDatabase[], int *pNrOfPatients, int *pNrOfPatientsFound, int *pPatientFound);
+void sortSelect(Patient patientDatabase[], int *pNrOfPatients);
+void sortByName(Patient patientDatabase[], int *pNrOfPatients);
+void sortByidNum(Patient patientDatabase[], int *pNrOfPatients);
+void addPictures(Patient patientDatabase[], int *pNrOfPatients);
+void removePatient(Patient patientDatabase[], int *pNrOfPatients);
+void saveToFile(Patient patientDatabase[], int *pNrOfPatients, char fileName[]);
+void readFromFile(Patient patientDatabase[], int *pNrOfPatients, char fileName[]);
+void selectFile(char fileName[]);
 
 int main(){
+	Patient patientDatabase[MAX];
+	int nrOfPatients=0;
+	char fileName[WORDLENGTH];
+	selectFile(fileName);
+	readFromFile(patientDatabase, &nrOfPatients, fileName);
+	databaseManagement(patientDatabase, &nrOfPatients, fileName);
+}
+
+void databaseManagement(Patient patientDatabase[], int *pNrOfPatients, char fileName[]){
 	char choice;
-	int measurements[LENGTH] ={0};
-	int nrOfMeasurements = 0;
-	
-	printf("Measurement tool 1.1\n");
 	while(choice != 'q'){
-		printf("VECRQ? ");
+		printf("Register new patients (n)\n");
+		printf("View all patients (v)\n");
+		printf("Search for patients (s)\n");
+		printf("Sort patients (z)\n");
+		printf("Add pictures (a)\n");
+		printf("Remove patient (r)\n");
+		printf("Quit program (q)\n");
 		scanf(" %c", &choice);
-		
 		switch(choice){
-			case 'v': viewData(measurements, nrOfMeasurements);
+			case 'n': regPatients(patientDatabase, pNrOfPatients);
 			break;
-			case 'e': nrOfMeasurements = enterData(measurements, nrOfMeasurements);
+			case 'v': printDatabase(patientDatabase, pNrOfPatients);
 			break;
-			case 'c': compute(measurements, nrOfMeasurements);
+			case 's': searchSelect(patientDatabase, pNrOfPatients);
 			break;
-			case 'r': nrOfMeasurements = reset(measurements, nrOfMeasurements);
+			case 'z': sortSelect(patientDatabase, pNrOfPatients);
 			break;
-			case 'q': 
+			case 'a': addPictures(patientDatabase, pNrOfPatients);
+			break;
+			case 'r': removePatient(patientDatabase, pNrOfPatients);
+			break;
+			case 'q': saveToFile(patientDatabase, pNrOfPatients, fileName);
 			break;
 		}
 	}
 }
 
-	int enterData(int measurements[], int nrOfMeasurements){
+void regPatients(Patient patientDatabase[] , int *pNrOfPatients){
+	char again[WORDLENGTH] = "yes", name[WORDLENGTH], tmp[WORDLENGTH], idNum[WORDLENGTH];
+	int picRef[WORDLENGTH];
+	gets(tmp);
+	while(strcmp(again,"yes") == 0){
+		if(*pNrOfPatients<MAX){
+			printf("Enter name: ");
+			gets(name);
+			printf("Enter id number: ");
+			gets(idNum);
+			int nrOfPics=0;
+			for(int i=0;i<WORDLENGTH;i++){
+				printf("Enter picRef: ");
+				scanf("%d", &picRef[i]);
+				if(picRef[i]==0){
+					break;
+				}
+				nrOfPics++;
+				if(nrOfPics>=10){
+					printf("You have entered the maximum number of pictures\n");
+					break;
+				}
+			}
+			gets(tmp);
+			patientDatabase[*pNrOfPatients]=createPatient(name, idNum, picRef, nrOfPics);
+			(*pNrOfPatients)++;
+			printf("Do you want to continue? (yes/no) ");
+			gets(again);
+		}
+	}
+}
 
-		for(int i=nrOfMeasurements;i<LENGTH;i++){
-			printf("Enter measurement #%d (or 0)? ", nrOfMeasurements + 1);
-			scanf("%d", &measurements[i]);
-			if(measurements[i] == 0){
+Patient createPatient(char name[WORDLENGTH], char idNum[], int picRef[], int nrOfPics){
+	Patient p;
+	strcpy(p.name, name);
+	strcpy(p.idNum, idNum);
+	for(int i=0;i<nrOfPics;i++){
+		p.picRef[i]=picRef[i];
+	}
+	p.nrOfPics=nrOfPics;
+return p;
+}
+
+void printDatabase(Patient patientDatabase[], int *pNrOfPatients){
+	printMenu();
+	for(int i=0;i<*pNrOfPatients;i++){
+		printf("%s\t%s\t", patientDatabase[i].idNum, patientDatabase[i].name);
+		printf("[ ");
+		for(int j=0;j<patientDatabase[i].nrOfPics;j++){
+			printf("%d ",  patientDatabase[i].picRef[j]);
+		}
+		printf("]");
+		printf("\n\n");
+	}
+}
+
+void printPatient(Patient p){
+	printf("%s\t%s\t", p.idNum, p.name);
+	printf("[ ");
+	for(int i=0;i<p.nrOfPics;i++){
+			printf("%d ",  p.picRef[i]);
+		}
+	printf("]");
+	printf("\n\n");
+}
+
+void printMenu(){
+	printf("ID-number\t Name\t\t Image references\n");
+	printf("-----------------------------------------------------");
+	printf("\n");
+}
+
+void sortSelect(Patient patientDatabase[], int *pNrOfPatients){
+	char choice;
+	printf("Select sorting method\n");
+	printf("By name (n) ");
+	printf("By id number (i) ");
+	scanf(" %c", &choice);
+	switch(choice){
+		case 'n': sortByName(patientDatabase, pNrOfPatients);
+		break;
+		case 'i': sortByidNum(patientDatabase, pNrOfPatients);
+		break;
+	}
+}
+
+void sortByName(Patient patientDatabase[], int *pNrOfPatients){
+	Patient tmp;
+	for(int i=0;i<*pNrOfPatients-1;i++){
+		for(int j=0;j<*pNrOfPatients-1;j++){
+			if(strcmp(patientDatabase[j].name, patientDatabase[j+1].name)>0){
+				tmp=patientDatabase[j];
+				patientDatabase[j]=patientDatabase[j+1];
+				patientDatabase[j+1]=tmp;
+			}
+		}
+	}
+}
+
+void sortByidNum(Patient patientDatabase[], int *pNrOfPatients){
+	Patient tmp;
+	for(int i=0;i<*pNrOfPatients-1;i++){
+		for(int j=0;j<*pNrOfPatients-1;j++){
+			if(strcmp(patientDatabase[j].idNum, patientDatabase[j+1].idNum)>0){
+				tmp=patientDatabase[j];
+				patientDatabase[j]=patientDatabase[j+1];
+				patientDatabase[j+1]=tmp;
+			}
+		}
+	}
+}
+
+int searchSelect(Patient patientDatabase[], int *pNrOfPatients){
+	char choice;
+	int nrOfPatientsFound=0;
+	int patientFound;
+	printf("Search for name(n), id number(i) or Reference picture(p)? ");
+	scanf(" %c", &choice);
+	switch(choice){
+		case 'n': searchName(patientDatabase, pNrOfPatients, &nrOfPatientsFound, &patientFound);
+		break;
+		case 'i': searchId(patientDatabase, pNrOfPatients, &nrOfPatientsFound, &patientFound);
+		break;
+		case 'p': searchPicRef(patientDatabase, pNrOfPatients, &nrOfPatientsFound, &patientFound);
+		break;
+	}
+	if(nrOfPatientsFound==1){
+		return patientFound;
+	}
+	else{
+		patientFound=10001;
+		return patientFound;
+	}
+}
+
+ void searchName(Patient patientDatabase[], int *pNrOfPatients, int *pNrOfPatientsFound, int *pPatientFound){
+	char searchWord[WORDLENGTH];
+	scanf("%s", searchWord);
+	printMenu();
+	for(int i=0;i<*pNrOfPatients;i++){
+		if(strstr(patientDatabase[i].name, searchWord)){
+			printPatient(patientDatabase[i]);
+			(*pNrOfPatientsFound)++;
+			*pPatientFound=i;
+		}
+	}
+}
+
+void searchId(Patient patientDatabase[], int *pNrOfPatients, int *pNrOfPatientsFound, int *pPatientFound){
+	char searchWord[WORDLENGTH];
+	scanf("%s", searchWord);
+	printMenu();
+	for(int i=0;i<*pNrOfPatients;i++){
+		if(strcmp(patientDatabase[i].idNum, searchWord)==0){
+			printPatient(patientDatabase[i]);
+			(*pNrOfPatientsFound)++;
+			*pPatientFound=i;
+		}
+	}
+}
+
+void searchPicRef(Patient patientDatabase[], int *pNrOfPatients, int *pNrOfPatientsFound, int *pPatientFound){
+	int searchNum;
+	scanf("%d", &searchNum);
+	printMenu();
+	for(int i=0;i<*pNrOfPatients;i++){
+		for(int j=0;j<patientDatabase[i].nrOfPics;j++)
+		if(patientDatabase[i].picRef[j]==searchNum){
+			printPatient(patientDatabase[i]);
+			(*pNrOfPatientsFound)++;
+			*pPatientFound=i;
+		}
+	}
+}
+
+void addPictures(Patient patientDatabase[], int *pNrOfPatients){
+	int patientFound;
+	char choice;
+	patientFound=searchSelect(patientDatabase, pNrOfPatients);
+	if(patientFound<10000){
+		for(int i=patientDatabase[patientFound].nrOfPics;i<WORDLENGTH;i++){
+			if(patientDatabase[patientFound].nrOfPics>=10){
+				printf("You have entered the maximum number of pictures\n");
 				break;
 			}
-			nrOfMeasurements++;
+			printf("Enter picRef: ");
+			scanf("%d", &patientDatabase[patientFound].picRef[i]);
+			if(patientDatabase[patientFound].picRef[i]==0){
+				break;
+			}
+			patientDatabase[patientFound].nrOfPics++;
 		}
-	return nrOfMeasurements;
-	
 	}
-	
-	void viewData(int measurements[], int nrOfMeasurements){
-		
-		if(measurements[0] != 0){
-			for(int i=0;i<nrOfMeasurements;i++){
-				printf("%d ", measurements[i]);
+	printf("Do you want to search again? (y/n) ");
+	scanf(" %c", &choice);
+	if(choice=='y'){
+		addPictures(patientDatabase, pNrOfPatients);
+	}
+}
+
+void removePatient(Patient patientDatabase[], int *pNrOfPatients){
+	int patientFound;
+	char choice;
+	patientFound=searchSelect(patientDatabase, pNrOfPatients);
+	if(patientFound<10000){
+		printf("Do you wish to remove this patient? (y/n) ");
+		scanf(" %c", &choice);
+		if(choice=='y'){
+			for(int i=patientFound;i<*pNrOfPatients-1;i++){
+				patientDatabase[i] = patientDatabase[i+1];
+			}
+			(*pNrOfPatients)--;
+		}
+	}
+	printf("Do you want to search again? (y/n) ");
+	scanf(" %c", &choice);
+	printf("\n");
+	if(choice=='y'){
+		removePatient(patientDatabase, pNrOfPatients);
+	}
+}
+
+void saveToFile(Patient patientDatabase[], int *pNrOfPatients, char fileName[]){
+	FILE *fp;
+	fp=fopen(fileName,"w");
+	if(fp!=NULL){
+		for(int i=0;i<*pNrOfPatients;i++){
+			fprintf(fp,"%s %s %d\n", patientDatabase[i].name, patientDatabase[i].idNum, patientDatabase[i].nrOfPics);
+			for(int j=0;j<patientDatabase[i].nrOfPics;j++){
+				fprintf(fp,"%d\n", patientDatabase[i].picRef[j]);
 			}
 		}
-		else{
-			printf("No measurements");
-		}
-		printf("\n");
+		fclose(fp);
 	}
-	
-	int reset(int measurements[], int nrOfMeasurements){
-		
-		for(int i=0;i<nrOfMeasurements;i++){
-		measurements[i] = 0;
-	}
-	nrOfMeasurements = 0;
-	return nrOfMeasurements;
-	}
-	
-	void compute(int measurements[], int nrOfMeasurements){
-		
-		if(measurements[0] != 0){
-			int max;
-			int min;
-			float avr;
-			int tempArr[nrOfMeasurements];
-		
-			max = calcMax(measurements, nrOfMeasurements);
-			min = calcMin(measurements, nrOfMeasurements);
-			avr = calcAvr(measurements, nrOfMeasurements);
-			copyArr(measurements, tempArr, nrOfMeasurements);
-			calcNorm(tempArr, nrOfMeasurements);
-			
-			printf("Max value: %d\n", max);
-			printf("Min value: %d\n", min);
-			printf("Avr value: %f\n", avr);
-			viewData(tempArr, nrOfMeasurements);
-		}
-		else{
-			printf("No measurements\n");
-		}
-	}
-	
-	int calcMax(int measurements[], int nrOfMeasurements){
-		
-		int max;
-		max = measurements[0];
-		for(int i=1;i<nrOfMeasurements;i++){
-			if(measurements[i]>max){
-				max = measurements[i];
+}
+
+void readFromFile(Patient patientDatabase[], int *pNrOfPatients, char fileName[]){
+	FILE *fp;
+	fp=fopen(fileName,"r");
+	if(fp!=NULL){
+		char firstName[WORDLENGTH], lastName[WORDLENGTH], idNum[WORDLENGTH]; 
+		int picRef[MAXPICTURES];
+		int nrOfPics=0, length, tmp;
+		while(fscanf(fp,"%s %s %s %d", firstName, lastName, idNum, &nrOfPics)==4){
+			for(int i=0;i<nrOfPics;i++){
+				fscanf(fp,"%d", &picRef[i]);
+			}
+			length = strlen(firstName);
+			firstName[length]=' ';
+			firstName[length+1]='\0';
+			strcat(firstName ,lastName);
+			patientDatabase[*pNrOfPatients] = createPatient(firstName, idNum, picRef, nrOfPics);
+			(*pNrOfPatients)++;
+			if(*pNrOfPatients>=MAX){
+				printf("The file is to large, entering the first 10000 patients\n");
+				break;
 			}
 		}
-		return max;
-		
+		fclose(fp);
 	}
-	
-	int calcMin(int measurements[], int nrOfMeasurements){
-		int min;
-		min = measurements[0];
-		for(int i=1;i<nrOfMeasurements;i++){
-			if(measurements[i]<min){
-				min = measurements[i];
-			}
-		}
-		return min;
-	}
-	
-	float calcAvr(int measurements[], int nrOfMeasurements){
-		float sum=0;
-		float avr=0;
-		for(int i=0;i<nrOfMeasurements;i++){
-			sum = sum + measurements[i];
-		}
-		avr = sum/nrOfMeasurements;
-		return avr;
-	}
-	
-	void copyArr(int measurements[], int tempArr[], int nrOfMeasurements){
-		
-		for(int i=0;i<nrOfMeasurements;i++){
-			tempArr[i] = measurements[i];
-		}
-	}
-	
-	void calcNorm(int tempArr[], int nrOfMeasurements){
-		float avr;
-		avr = calcAvr(tempArr, nrOfMeasurements);
-		
-		for(int i=0;i<nrOfMeasurements;i++){
-			tempArr[i] = roundf(tempArr[i] - avr);
-		}
-	}
+}
+
+void selectFile(char fileName[]){
+	printf("Pease enter a file to open: ");
+	gets(fileName);
+	printf("\n");
+}
--- Uppgifter/Temp.c
+++ Uppgifter/Temp.c
@@ -1,26 +1,109 @@
 #include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
-#include <time.h>
+#define WORDLENGTH 30
+#define MAX 100
+
+struct car{
+	char model[WORDLENGTH];
+	int year;
+	int milage;
+};
+typedef struct car Car;
 
 
-int main(void)
-{
-	int a[2];
-	int tmp;
-	
-	srand(time(NULL));
-	a[0] = rand();
-	a[1] = rand();
-	
-	if(a[0]>a[1])
- {
-		tmp=a[0];
-		a[0]=a[1];
-		a[1]=tmp;
- }
-	for(int i=0;i< 2;i++) {
-		printf("%d",a[i]);
-		printf("\n");
+
+void printCar(Car c);
+Car createCar(char model[],int year, int milage);
+void regCars(Car reg[], int *pNrOfCars);
+void printRegister(Car reg[], int nrOfCars);
+void saveToFile(Car reg[],int nrOfCars);
+void readFromFile(Car reg[],int *pNrOfCars);
+
+
+int main(){
+	Car carRegister[MAX];
+	int nrOfCars=0;
+	char choice;
+	readFromFile(carRegister, &nrOfCars);
+	while(choice != 'q'){
+		printf("Register cars (r)\n");
+		printf("View register (v)\n");
+		printf("Quit (q)\n");
+		scanf(" %c", &choice);
+		switch(choice){
+			case 'r': regCars(carRegister,&nrOfCars);
+			break;
+			case 'v': printRegister(carRegister, nrOfCars);
+			break;
+			case 'q': saveToFile(carRegister, nrOfCars);
+			break;
+		}
+	}
 }
-	
+
+void printCar(Car c){
+	printf("Model: %s, Year: %d, Milage: %d\n",c.model,c.year,c.milage); 
+}
+
+Car createCar(char model[],int year, int milage){
+	Car c;
+	strcpy(c.model,model);
+	c.year=year;
+	c.milage=milage;
+	return c;
+}
+
+void regCars(Car reg[], int *pNrOfCars){
+	char again[WORDLENGTH] = "yes", model[WORDLENGTH], tmp[WORDLENGTH];
+	int year, milage;
+	gets(tmp);
+	while(strcmp(again,"yes") == 0){
+		printf("Enter model: ");
+		gets(model);
+		printf("Enter year: ");
+		gets(tmp);
+		year=atoi(tmp);
+		printf("Enter milage: ");
+		gets(tmp);
+		milage=atoi(tmp);
+		reg[*pNrOfCars]=createCar(model,year,milage);
+		(*pNrOfCars)++;
+		printf("Do you want to continue? (yes/no) ");
+		gets(again);
+	}
+}
+
+void printRegister(Car reg[], int nrOfCars){
+	for(int i=0;i<nrOfCars;i++){
+		printf("%d. Model: %s, Year: %d, Milage: %d\n", i+1, reg[i].model, reg[i].year, reg[i].milage);
+	}
+}
+
+void saveToFile(Car reg[],int nrOfCars)
+{
+	FILE *fp;
+	fp=fopen("minFil.txt","w");
+	if(fp!=NULL){
+		int year,milage;
+		for(int i=0;i<nrOfCars;i++){
+			fprintf(fp,"%s %d %d\n", reg[i].model, reg[i].year, reg[i].milage);
+		}
+		fclose(fp);
+	}
+}
+
+void readFromFile(Car reg[],int *pNrOfCars)
+{
+ FILE *fp;
+ fp=fopen("minFil.txt","r");
+ if(fp!=NULL){
+ char model[WORDLENGTH];
+ int year,milage;
+ while(fscanf(fp,"%s %d %d",model,&year,&milage)==3){
+ reg[*pNrOfCars] = createCar(model,year,milage);
+ (*pNrOfCars)++;
+ }
+ fclose(fp);
+ }
 }
